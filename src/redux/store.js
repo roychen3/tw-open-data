e2d72b7e48750697d524ddb06ef3dba6917d495a
unknown <Roy Chen@RoyChen.bankpro.com.tw>
2020-11-22 15:05:07 +0800

    Set store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import rootReducer from './reducers'
+
+const bindMiddleware = (middleware) => {
+    if (process.env.NODE_ENV !== 'production') {
+        return composeWithDevTools(applyMiddleware(...middleware))
+    }
+    return applyMiddleware(...middleware)
+}
 
 const initialState = {}
 
@@ -9,9 +18,7 @@ const middleware = [thunk]
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ))
+    bindMiddleware(middleware),
+)
 
 export default store

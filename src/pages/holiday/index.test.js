d91f199dce806bd95221601773f5da476b462102
unknown <Roy Chen@RoyChen.bankpro.com.tw>
2021-07-28 21:53:27 +0800

    WIP

--- src/pages/holiday/index.test.js
+++ src/pages/holiday/index.test.js
@@ -1,8 +1,11 @@
-import { unmountComponentAtNode } from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from "react-dom/test-utils"
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import fetchMock from 'fetch-mock'
 import expect from 'expect' // You can use any testing library
+
 import {
     getHoliday,
     getHolidaySuccess,
@@ -15,6 +18,7 @@ import {
 } from '../../constants/actionTypes'
 
 import fakeApiData from './fakeData.json'
+import Holiday from './index'
 
 let container = null
 beforeEach(() => {
@@ -36,161 +40,6 @@ const mockStore = configureMockStore(middlewares)
 describe('async actions', () => {
     afterEach(() => {
         fetchMock.restore()
-    })
-
-    it('produce api result data', () => {
-        const produceApiResultData = (data) => {
-            const filterData = data.filter((item) => item.holidayCategory !== '星期六、星期日')
-
-            return filterData.map((item) => ({
-                id: item.date,
-                date: item.date,
-                name: item.name,
-                isHoliday: item.isHoliday,
-                holidayCategory: item.holidayCategory,
-                description: item.description,
-            }))
-        }
-
-        const fakeApiData = {
-            success: true,
-            result: {
-                resource_id: '382000000A-000077-002',
-                limit: 2000,
-                total: 958,
-                fields: [
-                    {
-                        type: 'text',
-                        id: 'date'
-                    },
-                    {
-                        type: 'text',
-                        id: 'name'
-                    },
-                    {
-                        type: 'text',
-                        id: 'isHoliday'
-                    },
-                    {
-                        type: 'text',
-                        id: 'holidayCategory'
-                    },
-                    {
-                        type: 'text',
-                        id: 'description'
-                    }
-                ],
-                records: [
-                    {
-                        date: '2020/1/1',
-                        name: '中華民國開國紀念日',
-                        isHoliday: '是',
-                        holidayCategory: '放假之紀念日及節日',
-                        description: '全國各機關學校放假一日。',
-                    },
-                    {
-                        date: '2020/1/4',
-                        name: '',
-                        isHoliday: '是',
-                        holidayCategory: '星期六、星期日',
-                        description: '',
-                    },
-                    {
-                        date: '2020/1/23',
-                        name: '',
-                        isHoliday: '是',
-                        holidayCategory: '調整放假日',
-                        description: '',
-                    },
-                    {
-                        date: '2020/1/28',
-                        name: '',
-                        isHoliday: '是',
-                        holidayCategory: '補假',
-                        description: '',
-                    },
-                ]
-            }
-        }
-        const fakeTableData = [
-            {
-                id: '2020/1/1',
-                date: '2020/1/1',
-                name: '中華民國開國紀念日',
-                isHoliday: '是',
-                holidayCategory: '放假之紀念日及節日',
-                description: '全國各機關學校放假一日。',
-            },
-            {
-                id: '2020/1/23',
-                date: '2020/1/23',
-                name: '',
-                isHoliday: '是',
-                holidayCategory: '調整放假日',
-                description: '',
-            },
-            {
-                id: '2020/1/28',
-                date: '2020/1/28',
-                name: '',
-                isHoliday: '是',
-                holidayCategory: '補假',
-                description: '',
-            },
-        ]
-        expect(produceApiResultData(fakeApiData.result.records)).toStrictEqual(fakeTableData)
-    })
-
-    it('produce holiday year list', () => {
-        const produceHolidayYearList = (data) => {
-            const yearKeys = {}
-            data.forEach((item) => {
-                const year = String(new Date(item.date).getFullYear())
-                yearKeys[year] = year
-            })
-            return Object.keys(yearKeys).map((key) => ({ value: key, name: key }))
-        }
-        const fakeTableData = [
-            {
-                id: '2019/2/5',
-                date: '2019/2/5',
-                name: '春節',
-                isHoliday: '是',
-                holidayCategory: '放假之紀念日及節日',
-                description: '全國各機關學校於二月五日至二月七日放假三日，其中二月七日適逢星期四，調整二月八日為放假日，並於一月十九日補行上班一日。'
-            },
-            {
-                id: '2020/1/23',
-                date: '2020/1/23',
-                name: '',
-                isHoliday: '是',
-                holidayCategory: '調整放假日',
-                description: ''
-            },
-            {
-                id: '2021/12/31',
-                date: '2021/12/31',
-                name: '',
-                isHoliday: '是',
-                holidayCategory: '補假',
-                description: '111年開國紀念日(一月一日)適逢星期六，於十二月三十一日補假。'
-            }
-        ]
-        const fakeHolidayYearList = [
-            {
-                value: '2019',
-                name: '2019'
-            },
-            {
-                value: '2020',
-                name: '2020'
-            },
-            {
-                value: '2021',
-                name: '2021'
-            }
-        ]
-        expect(produceHolidayYearList(fakeTableData)).toStrictEqual(fakeHolidayYearList)
     })
 
     it('creates GET_HOLIDAY_SUCCESS when fetching holiday has been done', async () => {
@@ -292,5 +141,19 @@ describe('async actions', () => {
 
         // remove the mock to ensure tests are completely isolated
         global.fetch.mockRestore()
+    })
+
+    it('test', async () => {
+        const store = mockStore({
+            holiday: {
+                holidayYearList: [],
+                holiday: [],
+                holidayError: null,
+                holidayLoading: false,
+            },
+        })
+        act(() => {
+            render(<Provider store={store}><Holiday /></Provider>, container)
+        })
     })
 })

b5e7a2775103c9a210c99155f463250e2c8ff184
Roy Chen <roy850323@gmail.com>
2022-01-02 19:13:36 +0800

    Add `needFullWidth` For Page Container.

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -14,37 +14,15 @@ padding-top: 62px; /* header height */
 margin-bottom: -51px; /* footer height */
 `
 
-const StyledContentContainer = styled.div`
-width: 95%;
-margin: 0 auto;
-padding: 4rem 0 8rem;
-
-@media (min-width: 960px) {
-    width: 70%;
-}
-
-.page-title {
-    font-size: xx-large;
-    font-weight: bolder;
-    margin-bottom: 2rem;
-}
-
-.table-container {
-    margin: 1rem 0;
-}
-`
-
 const Content = () => {
     const routeList = creatRouteList()
 
     return (
         <StyledContent>
-            <StyledContentContainer>
-                <Switch>
-                    {routeList}
-                    <Redirect from="/" to={`/${menuList[0].hashName}`} />
-                </Switch>
-            </StyledContentContainer>
+            <Switch>
+                {routeList}
+                <Redirect from="/" to={`/${menuList[0].hashName}`} />
+            </Switch>
         </StyledContent>
     )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,29 @@
 import { lazy, useMemo } from 'react'
 import { Route } from "react-router-dom"
 import { useStore } from 'react-redux'
+import styled from 'styled-components'
 
 import { menuList } from '../constants/menuList'
+
+const StyledPageContainer = styled.div`
+width: 99%;
+margin: 0 auto;
+padding: 4rem 0 8rem;
+
+@media (min-width: 960px) {
+    width: ${({ $needFullWidth }) => $needFullWidth ? '99%' : '70%'};
+}
+
+.page-title {
+    font-size: xx-large;
+    font-weight: bolder;
+    margin-bottom: 2rem;
+}
+
+.table-container {
+    margin: 1rem 0;
+}
+`
 
 export const creatRouteList = () => {
     const store = useStore()
@@ -41,7 +62,19 @@ export const creatRouteList = () => {
     }
 
     const routeList = menuList.map((item) => (
-        <Route exact key={`${item.hashName}`} path={`/${item.hashName}`}>{getPageComponents(item.hashName)}</Route>))
+        <Route exact key={`${item.hashName}`} path={`/${item.hashName}`}>
+            {['taipeiSpeedCameraPositions'].includes(item.hashName)
+                ?
+                <StyledPageContainer $needFullWidth>
+                    {getPageComponents(item.hashName)}
+                </StyledPageContainer>
+                :
+                <StyledPageContainer>
+                    {getPageComponents(item.hashName)}
+                </StyledPageContainer>
+            }
+        </Route>
+    ))
 
     return useMemo(() => routeList, [])
 }

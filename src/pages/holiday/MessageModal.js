da5bac52542d36083b24b3ef39e394f46a00ed2f
Roy Chen <32289972+roychen3@users.noreply.github.com>
2023-12-07 14:51:51 +0800

    Update holiday fake data 2023 (#2)

--- src/pages/holiday/MessageModal.js
+++ src/pages/holiday/MessageModal.js
@@ -18,7 +18,7 @@ const MessageModal = () => {
       <StyledMessageContent>
         <p>因為 api 不支援跨網域，且來源只提供 csv 檔，呈現資料為手動更新，故有可能不會有最新資料。</p>
         <br />
-        <p>資料最後更新日期: 2021-12-18</p>
+        <p>資料最後更新日期: 2023-12-07</p>
         <p>資料來源: </p>
         <p><a href="https://data.gov.tw/dataset/145708">政府資料開放平臺</a></p>
         <p><a href="https://data.ntpc.gov.tw/datasets/308DCD75-6434-45BC-A95F-584DA4FED251">新北市政府資料開放平臺</a></p>
